51ac44842af98bd9059969fbb5b513eba85560f8
Kyle <49769178+kstoneberg@users.noreply.github.com>
2019-05-04 10:44:20 -0700

    Add files via upload

---
+++ Inventory.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class Inventory {
+
+    LinkedList<Item> inventory = new LinkedList<Item>();
+    String name;
+
+    Inventory(){}
+
+    public boolean isEmpty() {
+        return (inventory.size() == 0);
+    }
+
+    public void add(Item item){
+        inventory.add(item);
+    }
+
+    public void remove(Item item){
+        inventory.remove(item);
+    }
+
+    public void clear(){
+        inventory.clear();
+    }
+
+    public String toString(){
+        return inventory.toString();
+    }
+
+
+}
