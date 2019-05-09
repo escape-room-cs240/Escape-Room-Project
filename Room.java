aa8aa65dbc072311c147095bda0816bb0a380126
anderbc41 <brycecandersen@gmail.com>
2019-05-09 14:43:26 -0700

    Revised room class, functionality demo in escapeMain
    Removed other constructors from the room class, we can just do null for null rooms.

--- Room.java
+++ Room.java
@@ -1,32 +1,25 @@
+/*To-Do
+   -Store all previous room descriptions in a linked-list format
+*/
 public class Room{
    
-   private String desc;   
-   public Room prev;
-   public Room next;
+   private String desc;//Reference to the description of the room
+   public Room prev;//Reference to the previous room
+   public Room next;//Reference to the next room
    
-   
+   //Constructor for an instance of the Room class
    public Room(String desc, Room prev, Room next){
       this.desc = desc;
       this.prev = prev;
       this.next = next;
    }
    
-   public Room(String desc, Room next){
-      this.desc = desc;
-      this.prev = null;
-      this.next = next;
-   }
-   
-   public Room(String desc){
-      this.desc = desc;
-      this.prev = null;
-      this.next = null;
-   }
-   
+   //Changes the description of a given room 
    public void setDescription(String desc){
       this.desc = desc;
    }
    
+   //Returns the current description of a room
    public String getDescription(){
       return desc;
    }
--- escapeMain.java
+++ escapeMain.java
@@ -2,6 +2,10 @@ public class escapeMain{
 
     public static void main(String[] args) {
         Room dungeon = new Room("This is a dark room");
+        Room dungeon2 = new Room("This is another dark room");
+        dungeon.next = dungeon2;
+        dungeon2.prev = dungeon;
         System.out.println(dungeon.getDescription());
+        System.out.println(dungeon.next.getDescription());
     }
 }
